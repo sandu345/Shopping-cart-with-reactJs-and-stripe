5a86814fd6a5f526147adf2ff9a3d549c8fe5052
sandu345 <sandunidevindya04@gmail.com>
2023-06-17 10:03:17 +0530

    products are added tto the store.

---
+++ store/src/App.js
@@ -0,0 +1,31 @@
+import logo from './logo.svg';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import NavbarComponent from './components/navbar';
+import { Container } from 'react-bootstrap';
+import { BrowserRouter, Rotes, Route, Routes } from 'react-router-dom';
+import Store from './pages/Store';
+import Cancel from './pages/Cancel';
+import Success from './pages/Success';
+
+//localhost:3000 -> Home
+//localhost:3000/success -> Success
+
+function App() {
+  return (
+   
+    <Container>
+      <NavbarComponent></NavbarComponent>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Store />} />
+          <Route path="success" element={<Success />} />
+          <Route path="cancel" element={<Cancel />} />
+        </Routes>
+      </BrowserRouter>
+    </Container>
+    
+  );
+}
+
+export default App;
